7f3be2beac713eda900deb45f2fe73df3b94dc02
Gregory Duthé <duthe@ibk.baug.ethz.ch>
2021-11-23 11:37:42 +0100

    updated runner

--- gmsh_foil_run.py
+++ gmsh_foil_run.py
@@ -3,13 +3,13 @@ import sys
 import argparse
 
 parser = argparse.ArgumentParser()
+parser.add_argument('--input-dat-file', '-i', help="input file containing airfoil coordinates", type= str, default = None)
 parser.add_argument('--foil-number-string', '-f', help="the foil number", type= str, default = '4812')
-parser.add_argument('--angle-of-attack', '-a', help="angle of attack in radians", type= float, default= 0.)
-parser.add_argument('--output-mesh-file-name', '-o',help='the file name of the mesh output (without the .msh suffix)', type = str, default = 'mesh_out')
+parser.add_argument('--output-mesh-file-name', '-o',help='the file name of the mesh output (without the .msh suffix)', type = str, default = None)
 parser.add_argument('--view','-v',help='view the mesh after creating it',type=bool)
 
-def _gf_mesh_run(foil_number_string, angle_of_attack, output_mesh_file, view):
-    g = GMSHFoil(foil_name = foil_number_string, mesh_name=output_mesh_file)
+def _gf_mesh_run(input_dat_file, foil_number_string, output_mesh_file, view):
+    g = GMSHFoil(foil_dat_file=input_dat_file, naca_foil=foil_number_string, mesh_name=output_mesh_file)
     g.create_2d_unstructured_foil_mesh()
     if view:
         g.view()
@@ -22,7 +22,8 @@ if __name__ == '__main__':
         parser.print_help()
         exit()
     
-    _gf_mesh_run(args.foil_number_string,
-            args.angle_of_attack, 
+    _gf_mesh_run(
+            args.input_dat_file,
+            args.foil_number_string,
             args.output_mesh_file_name, 
             args.view)
